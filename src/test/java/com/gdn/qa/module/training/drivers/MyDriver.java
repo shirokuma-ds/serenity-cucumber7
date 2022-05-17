2b6734ff894d9b5bb78fc4c345843cd3f8c0dbcb
GDN\dennis.sandjaya <dennis.sandjaya@gdn-commerce.com>
2022-05-17 16:12:54 +0700

    Add Remote WebDriver

--- src/test/java/com/gdn/qa/module/training/drivers/MyDriver.java
+++ src/test/java/com/gdn/qa/module/training/drivers/MyDriver.java
@@ -4,12 +4,37 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import net.thucydides.core.webdriver.DriverSource;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MyDriver implements DriverSource {
     @Override
     public WebDriver newDriver() {
-        WebDriverManager.chromedriver().setup();
-        return new ChromeDriver();
+//        WebDriverManager.chromedriver().setup();
+//        return new ChromeDriver();
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("browserName", "chrome");
+        capabilities.setCapability("browserVersion", "92.0");
+        Map<String, Object> selenoidOptions = new HashMap<>();
+        selenoidOptions.put("enableVnc", true);
+        selenoidOptions.put("enableVideo", true);
+        capabilities.setCapability("selenoid:options", selenoidOptions);
+        RemoteWebDriver driver = null;
+        try {
+            driver = new RemoteWebDriver(
+                    URI.create("http://192.168.1.149:4444/wd/hub").toURL(),
+                    capabilities
+            );
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        return driver;
     }
 
     @Override
